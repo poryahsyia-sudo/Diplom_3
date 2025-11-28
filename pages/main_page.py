b9e10c4a2e7833d439700002076af5db6b0f44c4
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-11-28 20:44:39 +0500

    добавлен доступ к локаторам через self.locators в MainPage

--- pages/main_page.py
+++ pages/main_page.py
@@ -6,6 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class MainPage(BasePage):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.locators = MainPageLocators  # доступ к локаторам через self.locators
 
     @allure.step("Открытие главной страницы")
     def open(self, url):
@@ -13,30 +16,30 @@ class MainPage(BasePage):
 
     @allure.step("Переход в конструктор бургеров")
     def go_to_constructor(self):
-        self.click(MainPageLocators.CONSTRUCTOR_NAV_BUTTON)
+        self.click(self.locators.CONSTRUCTOR_NAV_BUTTON)
 
     @allure.step("Переход в ленту заказов")
     def go_to_order_feed(self):
-        element = self.find(MainPageLocators.ORDER_FEED_BUTTON)
+        element = self.find(self.locators.ORDER_FEED_BUTTON)
         ActionChains(self.driver).move_to_element(element).perform()
-        self.wait_until(EC.element_to_be_clickable(MainPageLocators.ORDER_FEED_BUTTON))
-        self.click(MainPageLocators.ORDER_FEED_BUTTON)
+        self.wait_until(EC.element_to_be_clickable(self.locators.ORDER_FEED_BUTTON))
+        self.click(self.locators.ORDER_FEED_BUTTON)
 
     @allure.step("Открытие модального окна ингредиента")
     def open_ingredient_modal(self):
-        self.click(MainPageLocators.FIRST_INGREDIENT)
+        self.click(self.locators.FIRST_INGREDIENT)
 
     @allure.step("Закрытие модального окна ингредиента")
     def close_ingredient_modal(self):
-        self.click(MainPageLocators.MODAL_CLOSE_BUTTON)
+        self.click(self.locators.MODAL_CLOSE_BUTTON)
 
     @allure.step("Добавление ингредиента в конструктор ({position})")
     def add_ingredient_to_constructor(self, position="top"):
-        ingredient = self.find(MainPageLocators.FIRST_INGREDIENT)
+        ingredient = self.find(self.locators.FIRST_INGREDIENT)
         constructor = self.find(
-            MainPageLocators.CONSTRUCTOR_TOP
+            self.locators.CONSTRUCTOR_TOP
             if position == "top"
-            else MainPageLocators.CONSTRUCTOR_BOTTOM
+            else self.locators.CONSTRUCTOR_BOTTOM
         )
         self.drag_and_drop(ingredient, constructor)
 
@@ -58,17 +61,17 @@ class MainPage(BasePage):
 
     @allure.step("Ожидание появления номера заказа")
     def wait_for_order_number(self, timeout=30):
-        self.wait_for_visibility(MainPageLocators.ORDER_NUMBER, timeout)
+        self.wait_for_visibility(self.locators.ORDER_NUMBER, timeout)
 
     @allure.step("Получение номера заказа")
     def get_order_number(self, timeout=30):
-        self.wait_for_visibility(MainPageLocators.ORDER_NUMBER, timeout)
-        return self.get_text(MainPageLocators.ORDER_NUMBER, timeout)
+        self.wait_for_visibility(self.locators.ORDER_NUMBER, timeout)
+        return self.get_text(self.locators.ORDER_NUMBER, timeout)
 
     @allure.step("Нажатие кнопки 'Оформить заказ'")
     def place_order(self):
-        self.click(MainPageLocators.PLACE_ORDER_BUTTON)
+        self.click(self.locators.PLACE_ORDER_BUTTON)
 
     @allure.step("Закрытие модального окна заказа")
     def close_order_modal(self):
-        self.click(MainPageLocators.MODAL_CLOSE_BUTTON)
+        self.click(self.locators.MODAL_CLOSE_BUTTON)
