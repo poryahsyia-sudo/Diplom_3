8c50711c9efa5c3beb642a647a19cf8c42c2725a
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-11-28 20:40:44 +0500

    добавлены шаги allure и перенос всех обращений к driver/WebDriverWait в методы BasePage

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,82 +1,74 @@
+import allure
 from pages.base_page import BasePage
 from pages.locators import MainPageLocators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
-from pages.urls import BASE_URL
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class MainPage(BasePage):
 
-    def open(self, url=BASE_URL):
-        self.driver.get(url)
+    @allure.step("Открытие главной страницы")
+    def open(self, url):
+        super().open(url)
 
+    @allure.step("Переход в конструктор бургеров")
     def go_to_constructor(self):
         self.click(MainPageLocators.CONSTRUCTOR_NAV_BUTTON)
 
+    @allure.step("Переход в ленту заказов")
     def go_to_order_feed(self):
         element = self.find(MainPageLocators.ORDER_FEED_BUTTON)
         ActionChains(self.driver).move_to_element(element).perform()
-        WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable(MainPageLocators.ORDER_FEED_BUTTON)
-        )
-        try:
-            element.click()
-        except Exception:
-            self.driver.execute_script("arguments[0].click();", element)
+        self.wait_until(EC.element_to_be_clickable(MainPageLocators.ORDER_FEED_BUTTON))
+        self.click(MainPageLocators.ORDER_FEED_BUTTON)
 
-
+    @allure.step("Открытие модального окна ингредиента")
     def open_ingredient_modal(self):
         self.click(MainPageLocators.FIRST_INGREDIENT)
 
+    @allure.step("Закрытие модального окна ингредиента")
     def close_ingredient_modal(self):
         self.click(MainPageLocators.MODAL_CLOSE_BUTTON)
 
+    @allure.step("Добавление ингредиента в конструктор ({position})")
     def add_ingredient_to_constructor(self, position="top"):
         ingredient = self.find(MainPageLocators.FIRST_INGREDIENT)
-        constructor = self.find(MainPageLocators.CONSTRUCTOR_TOP if position == "top" else MainPageLocators.CONSTRUCTOR_BOTTOM)
-        self.drag_and_drop_universal(ingredient, constructor)
+        constructor = self.find(
+            MainPageLocators.CONSTRUCTOR_TOP
+            if position == "top"
+            else MainPageLocators.CONSTRUCTOR_BOTTOM
+        )
+        self.drag_and_drop(ingredient, constructor)
 
+    @allure.step("Проверка видимости элемента")
     def is_visible(self, locator, timeout=5):
         try:
-            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+            self.wait_for_visibility(locator, timeout)
             return True
-        except:
+        except Exception:
             return False
 
+    @allure.step("Проверка невидимости элемента")
     def is_not_visible(self, locator, timeout=5):
         try:
-            WebDriverWait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+            self.wait_for_invisibility(locator, timeout)
             return True
-        except:
-            return False
-        
-
-    def drag_and_drop_universal(self, ingredient, constructor):
-        self.scroll_into_view(ingredient)
-        self.scroll_into_view(constructor)
-        js = """
-            const src = arguments[0];
-            const tgt = arguments[1];
-            const dataTransfer = new DataTransfer();
-            function fire(el, type, dt){
-                const e = new DragEvent(type, {
-                    bubbles: true,
-                    cancelable: true,
-                    dataTransfer: dt
-                });
-                el.dispatchEvent(e);
-            }
-            fire(src, 'dragstart', dataTransfer);
-            fire(tgt, 'dragenter', dataTransfer);
-            fire(tgt, 'dragover', dataTransfer);
-            fire(tgt, 'drop', dataTransfer);
-            fire(src, 'dragend', dataTransfer);
-        """
-        try:
-            self.driver.execute_script(js, ingredient, constructor)
         except Exception:
-            # Fallback: классический ActionChains
-            actions = ActionChains(self.driver)
-            actions.click_and_hold(ingredient).move_to_element(constructor).pause(0.2).release().perform()
+            return False
+
+    @allure.step("Ожидание появления номера заказа")
+    def wait_for_order_number(self, timeout=30):
+        self.wait_for_visibility(MainPageLocators.ORDER_NUMBER, timeout)
+
+    @allure.step("Получение номера заказа")
+    def get_order_number(self, timeout=30):
+        self.wait_for_visibility(MainPageLocators.ORDER_NUMBER, timeout)
+        return self.get_text(MainPageLocators.ORDER_NUMBER, timeout)
+
+    @allure.step("Нажатие кнопки 'Оформить заказ'")
+    def place_order(self):
+        self.click(MainPageLocators.PLACE_ORDER_BUTTON)
+
+    @allure.step("Закрытие модального окна заказа")
+    def close_order_modal(self):
+        self.click(MainPageLocators.MODAL_CLOSE_BUTTON)
