bc05053ee641626fd5be8593cb67007039ec9a36
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-11-28 20:41:37 +0500

    добавлены шаги allure и централизованы методы логина через BasePage

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,10 +1,28 @@
+import allure
 from pages.base_page import BasePage
 from pages.locators import LoginPageLocators
-from pages.locators import MainPageLocators
+
 
 class LoginPage(BasePage):
 
+    @allure.step("Авторизация пользователя с email: {email}")
     def login(self, email, password):
-        self.find(LoginPageLocators.EMAIL_FIELD).send_keys(email)
-        self.find(LoginPageLocators.PASSWORD_FIELD).send_keys(password)
+        self.fill_email(email)
+        self.fill_password(password)
+        self.submit_login()
+
+    @allure.step("Ввод email")
+    def fill_email(self, email):
+        field = self.find(LoginPageLocators.EMAIL_FIELD)
+        field.clear()
+        field.send_keys(email)
+
+    @allure.step("Ввод пароля")
+    def fill_password(self, password):
+        field = self.find(LoginPageLocators.PASSWORD_FIELD)
+        field.clear()
+        field.send_keys(password)
+
+    @allure.step("Нажатие кнопки 'Войти'")
+    def submit_login(self):
         self.click(LoginPageLocators.LOGIN_BUTTON)
