2dbe41d6f0867922ef58c2ded632d9fe6f30d8e1
Makiyenko Anastassiya <poryahsyia@gmail.com>
2025-11-29 16:41:24 +0500

    добавлено ожидание появления заказа в ленте заказов с повторной проверкой и увеличенным таймаутом

--- pages/order_feed_page.py
+++ pages/order_feed_page.py
@@ -3,6 +3,7 @@ from pages.base_page import BasePage
 from pages.locators import OrderFeedPageLocators
 from pages.helpers import to_int
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class OrderFeedPage(BasePage):
@@ -33,11 +34,15 @@ class OrderFeedPage(BasePage):
                 continue
         raise Exception("Не найден блок со счётчиком 'Выполнено за сегодня'")
 
-    @allure.step("Проверка, что заказ с номером {order_number} отображается в ленте")
-    def is_order_in_feed(self, order_number, timeout=30):
-        locator = (By.XPATH, f"//li[contains(text(), '{order_number}')]")
+    @allure.step("Проверка, что заказ с номером {order_number} отображается в ленте заказов")
+    def is_order_in_feed(self, order_number, timeout=60):
+        """Ожидает появления заказа с указанным номером в ленте заказов"""
+        locator = (By.XPATH, f"//*[contains(text(), '#{order_number}')]")
         try:
-            self.wait_for_visibility(locator, timeout)
+            WebDriverWait(self.driver, timeout).until(
+                lambda d: len(d.find_elements(*locator)) > 0,
+                f"Заказ #{order_number} не появился в ленте заказов за {timeout} секунд"
+            )
             return True
         except Exception:
             return False
